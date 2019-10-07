626e52595bbc392f2f35ed826073e9ef28430a4a
Max <maksymilianrozanski@gmail.com>
2019-10-07 14:08:53 +0200

    solve Tablice_Wielowymiarowe/Zad0

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -1,4 +1,3 @@
-
 var task1Array = [
     [2, 3],
     ["Ala", "Ola"],
@@ -13,3 +12,26 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+//Zadanie 0
+const arr = [
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+];
+
+function checkArray(array2d) {
+    let newArray = [];
+    for (let i = 0; i < array2d.length; i++) {
+        if (array2d[i][0] % 2 === 0) {
+            newArray.push(true)
+        } else {
+            newArray.push(false)
+        }
+    }
+    return newArray
+}
+
+console.log(checkArray(arr));
