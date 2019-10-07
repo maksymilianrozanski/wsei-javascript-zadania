862e101b50fe14a3d6c039cc79c1c6a3db2ae7aa
Max <maksymilianrozanski@gmail.com>
2019-10-07 13:20:46 +0200

    add solution: Zad1 Tablice

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -22,3 +22,17 @@ distFromAverage([2, 8, 3, 7]);
 console.log(distFromAverage([1, 2, 3, 4, 5, 6, 7]));
 console.log(distFromAverage([1, 1, 1, 1]));
 console.log(distFromAverage([2, 8, 3, 7]));
+
+//Zadanie 1
+const fruitsArray = ['blueberry', 'orange', 'strawberry'];
+
+function fruits(fruits) {
+    //first
+    console.log(fruits[0]);
+    //last
+    console.log(fruits[fruits.length - 1]);
+    //all
+    fruits.forEach(it => console.log(it.toString()))
+}
+
+fruits(fruitsArray);
