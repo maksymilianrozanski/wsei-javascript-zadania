a9fac64b75e54395b320387221e8590cbb9f205a
Max <maksymilianrozanski@gmail.com>
2019-10-09 14:59:14 +0200

    solve Obiekty Zad0a

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,11 @@
+//Zadanie 0a
+let city = {};
+city.capital = "Warsaw";
+city.population = 10000;
+city.president = "Mr. President";
+city.primeMinisters = ["Minister1", "Minister2"];
+
+console.log(city.capital);
+console.log(city.population);
+console.log(city.president);
+city.primeMinisters.forEach(it => console.log(it));
