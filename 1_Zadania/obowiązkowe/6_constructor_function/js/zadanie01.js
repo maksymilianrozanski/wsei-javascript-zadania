1b84a798e14844ecb06bc8af148c283b94fb41af
Max <maksymilianrozanski@gmail.com>
2019-10-14 11:45:20 +0200

    solve zadanie1

--- 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
@@ -1,20 +1,19 @@
 var Robot = function (name) {
     this.name = name;
     this.isFunctional = true;
-}
+};
 
 Robot.prototype.sayHi = function (toWho) {
-    if (isFunctional === true) {
+    if (this.isFunctional === true) {
         console.log("Robot " + this.name + " greets " + toWho);
-    }
-    else {
+    } else {
         console.log("Robot " + this.name + "is broken");
     }
 };
 
-Robot.prototype.changeName = function (newname) {
-    console.log("Robot " + this.name + "changes name to " + newname);
-    name = newname;
+Robot.prototype.changeName = function (newName) {
+    console.log("Robot " + this.name + "changes name to " + newName);
+    this.name = newName;
 };
 
 Robot.prototype.fixIt = function () {
