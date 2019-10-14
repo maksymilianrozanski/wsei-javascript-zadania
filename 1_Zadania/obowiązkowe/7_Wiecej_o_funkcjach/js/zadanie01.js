6152e42c623f2124a712cdd2e3c9d2868adb4f3c
Max <maksymilianrozanski@gmail.com>
2019-10-14 12:11:09 +0200

    solve zadanie1

--- 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie01.js
@@ -1,4 +1,3 @@
-
 /**
  * Zagniezdzanie funkcji.
  * Przesledz krok po kroku kazda linijke kodu. Kazda!
@@ -6,20 +5,28 @@
  */
 
 //Twoj komentarz ...
+//deklaracja funkcji
 function sortArray() {
 
     //Twoj komentarz ...
+    //deklaracja tablicy wewnątrz funkcji
     var points = [41, 3, 6, 1, 114, 54, 64];
 
     //Twoj komentarz ...
+    //wywołanie funkcji sortującej tablicę
     points.sort(function (a, b) {
         //Twoj komentarz ...
+        //w jaki sposób mają być porównywane wartości (zwracane mniejsze od zera, zero, lub większe od zera)
         return a - b;
     });
 
     //Twoj komentarz ...
+    //zwracanie posortowanej listy
     return points;
 }
 
 //Twoj komentarz ...
+//wywołanie funkcji
 sortArray();
+
+console.log(sortArray());
