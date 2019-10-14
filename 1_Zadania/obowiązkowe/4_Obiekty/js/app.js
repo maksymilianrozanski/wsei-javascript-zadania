b2bc5915bc2acda4f965abe5650315d20a19adbf
Max <maksymilianrozanski@gmail.com>
2019-10-14 10:53:00 +0200

    solve zadanie05

--- 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -69,3 +69,53 @@ var movie = {
 for (let each in movie) {
     console.log(each + ": " + movie[each.valueOf()])
 }
+
+//Zadanie 5
+var animals = [
+    {
+        type: "cat",
+        breed: "persian",
+        name: "Tiberius",
+        health: [
+            {
+                date: "2012-03-03",
+                visitType: "grafting"
+            },
+            {
+                date: "2015-06-23",
+                visitType: "bowel surgery"
+            }
+        ]
+    },
+    {
+        type: "Guinea pig",
+        breed: "The Rex",
+        name: "Marko",
+        health: [
+            {
+                date: "2015-12-04",
+                visitType: "grafting"
+            },
+            {
+                date: "2016-03-15",
+                visitType: "ear cleaning"
+            }
+        ]
+    }
+
+];
+console.log(" ");
+for (let i = 0; i < animals.length; i++) {
+    for (let each in animals[i]) {
+        if (!Array.isArray(animals[i][each.valueOf()])) {
+            console.log(each + ": " + animals[i][each.valueOf()])
+        } else {
+            for (let health in animals[i][each.valueOf()]) {
+                for (let property in animals[i][each.valueOf()][health]) {
+                    console.log(property.valueOf() + ": " + animals[i][each.valueOf()][health][property.valueOf()])
+                }
+            }
+        }
+    }
+    console.log(" ")
+}
