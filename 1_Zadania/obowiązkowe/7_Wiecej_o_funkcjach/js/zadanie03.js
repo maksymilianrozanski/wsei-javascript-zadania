09f96fb0978f758b20bf6e250b36877e3703a3be
Max <maksymilianrozanski@gmail.com>
2019-10-14 13:11:31 +0200

    solve zadanie3

---
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie03.js
@@ -0,0 +1,18 @@
+czesc();
+
+function czesc() {
+    console.log("Cześć");
+}
+
+czesc();
+
+// witaj();
+
+var witaj = function () {
+    console.log("Witaj");
+};
+
+witaj();
+
+//wywołanie funkcji nie przypisanej do zmiennej działa przed, oraz po jej deklaracji
+//Wywołanie funkcji przypisanej do zmiennej przed przypisaniem, powoduje błąd
