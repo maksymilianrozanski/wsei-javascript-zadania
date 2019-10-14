702f0c71d330342b29ade4793dc620a27ab22a0f
Max <maksymilianrozanski@gmail.com>
2019-10-14 11:39:07 +0200

    solve zadanie0

---
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -0,0 +1,25 @@
+//Zadanie 0
+function Basket() {
+    this.products = [];
+    this.sum = 0;
+    this.addProduct = function (name, price) {
+        this.products.push(name);
+        this.sum += price;
+    };
+    this.showBasket = function () {
+        console.log("Products: ");
+        console.log(this.products);
+        console.log("Sum: " + this.sum)
+    }
+}
+
+var aliceBasket = new Basket();
+aliceBasket.addProduct("pomidor", 10);
+aliceBasket.addProduct("arbuz", 40);
+aliceBasket.showBasket();
+
+var bruceBasket = new Basket();
+bruceBasket.addProduct("rice", 10);
+bruceBasket.addProduct("grzyby mun", 50);
+bruceBasket.addProduct("tofu", 20);
+bruceBasket.showBasket();
