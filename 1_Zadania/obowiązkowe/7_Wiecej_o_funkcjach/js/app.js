054d3ab20d1ebe14c7d65bb59cdaffd77ad990f3
Max <maksymilianrozanski@gmail.com>
2019-10-14 12:06:31 +0200

    solve zadanie0

--- 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/app.js
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/app.js
@@ -6,27 +6,35 @@
 
 
 //Twoj komentarz ...
+//Deklaracja funkcji jeden
 function jeden() {
 
     //Twoj komentarz ...
+    //Deklaracja zmiennej wewnątrz funcji jeden
     var zmienna1 = 1;
 
     //Twoj komentarz ...
+    //Deklaracja zmiennej dwa wewnątrz funkcji jeden
     function dwa() {
 
         //Twoj komentarz ...
+        //Wypisanie zmiennej zadeklarowanej w funkcji jeden
         console.log(zmienna1);
 
         //Twoj komentarz ...
+        //deklaracja funkcji zmienna2 wewnątrz funkcji dwa
         var zmienna2 = 3;
     }
 
     //Twoj komentarz ...
+    //wywołanie funcji dwa
     dwa();
 
     //Twoj komentarz ...
+    //próba wypisania wartości zmiennej 2, nieudana bo deklaracja zmiennej jest wewnątz funkcji wewnętrznej
     console.log(zmienna2)
 }
 
 //Twoj komentarz ...
-jeden()
+//wywołanie funkcji jeden
+jeden();
