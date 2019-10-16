19a7f34fcc7876ce0ab2c6f30008766673133b3e
Max <maksymilianrozanski@gmail.com>
2019-10-16 14:12:15 +0200

    solve Zadanie0/8_DOM

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+    //Zadanie 0
+    function getDataAnimationElement(element) {
+        let dataAnimation = element.getAttribute("data-animation");
+        return dataAnimation.valueOf();
+    }
+
+    let title = document.getElementsByClassName("title")[0];
+    getDataAnimationElement(title);
+
 });
