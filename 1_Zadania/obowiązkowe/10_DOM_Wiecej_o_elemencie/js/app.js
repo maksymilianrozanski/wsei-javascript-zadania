214cfb8fab19ade1c331ae8da22b5e35757b0692
Max <maksymilianrozanski@gmail.com>
2019-10-19 10:41:03 +0200

    solve Zadanie1/10_DOM

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -20,4 +20,25 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         elementNumber++;
     });
+
+    //Zadanie 1
+    let ex1 = document.getElementsByClassName("exercise ex1");
+    let chromeElement = ex1[0].getElementsByClassName("chrome");
+    chromeElement[0].style.width = "100px";
+    let chromeParent = chromeElement[0].parentNode;
+    let chromeAnchor = chromeParent.getElementsByTagName("a");
+    chromeAnchor[0].innerHTML = "Chrome";
+
+    let edgeElement = ex1[0].getElementsByClassName("edge");
+    edgeElement[0].style.backgroundImage = "url(assets/img/edge.png)";
+    let edgeParent = edgeElement[0].parentNode;
+    let edgeAnchor = edgeParent.getElementsByTagName("a");
+    edgeAnchor[0].href = "https://www.microsoft.com/en-us/windows/microsoft-edge";
+
+    let firefoxElement = ex1[0].getElementsByClassName("firefox");
+    firefoxElement[0].style.backgroundImage = "url(assets/img/firefox.png)";
+    let firefoxParent = firefoxElement[0].parentNode;
+    let firefoxAnchor = firefoxParent.getElementsByTagName("a");
+    firefoxAnchor[0].innerHTML = "Firefox";
+    firefoxAnchor[0].href = "https://www.mozilla.org/en-US/firefox/new/";
 });
