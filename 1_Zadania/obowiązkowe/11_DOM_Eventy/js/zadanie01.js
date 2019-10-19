22a217c2618cc5d2adcb2bda129ce6b20b2fe592
Max <maksymilianrozanski@gmail.com>
2019-10-19 20:09:54 +0200

    solve Zadanie01/11_DOM_Eventy

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,11 @@
-var menu = document.querySelector("#menu");
-var paragraf = document.querySelector("p");
+window.addEventListener("DOMContentLoaded", (event) => {
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+});
+
+// kod umieszczony w tagu head jest wykonywany przed wyswietlaniem strony
+//tak, umieszczenie eventu poprawilo sytuacje
+//ponowne przeniesienie tagu script do wnetrza tagu html nie powoduje zmian w wyswietlanej tresci
