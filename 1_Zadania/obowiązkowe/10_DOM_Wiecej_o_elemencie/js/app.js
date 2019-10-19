68c3b42ac9b0c401431707594d8f1e5421806157
Max <maksymilianrozanski@gmail.com>
2019-10-19 09:57:22 +0200

    solve Zadanie0/10_DOM

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    //Zadanie 0
+    let ex5 = document.getElementsByClassName("exercise ex5");
+    let listItems = ex5[0].getElementsByTagName("LI");
+    console.log(listItems);
+    let elementNumber = 0;
+    Array.from(listItems).forEach(it => {
+        if (elementNumber % 2 === 0) {
+            it.style.backgroundColor = "green";
+        }
+        if (elementNumber % 5 === 0){
+            it.className = "big"
+        }
+        if (elementNumber % 3 ===0){
+            it.style.textDecoration = "underline";
+        }
+        elementNumber++;
+    });
 });
