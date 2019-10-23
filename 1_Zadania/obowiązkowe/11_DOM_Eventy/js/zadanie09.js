896d9e1abf2a529aa6a64142cf062cc06c8ab0e2
Max <maksymilianrozanski@gmail.com>
2019-10-23 14:16:12 +0200

    solve Zadanie09/11_DOM_Eventy

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
@@ -1,28 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var a = document.getElementById("a");
-  var b = document.getElementById("b");
+    var a = document.getElementById("a");
+    var b = document.getElementById("b");
 
-  // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji
-  // addEventListener to właśnie document)
-  console.log("obiekt document: ", this);
+    // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji
+    // addEventListener to właśnie document)
+    console.log("obiekt document: ", this);
 
-  a.addEventListener("click", function (event) {
-    // Tutaj this wskazuje na element DOM o id a
-    console.log("a: ", this);
-  });
+    a.addEventListener("click", function (event) {
+        // Tutaj this wskazuje na element DOM o id a
+        console.log("a: ", this);
+    });
 
-  b.addEventListener("click", function (event) {
-    // Tutaj this wskazuje na element DOM o id b
-    console.log("b: ", this);
+    b.addEventListener("click", function (event) {
+        // Tutaj this wskazuje na element DOM o id b
+        console.log("b: ", this);
 
+//Można przekazać element jako argument jak w poniższym kodzie,
+// lub wyszukać element na nowo z użyciem getElementById
+        function innerFuncOne(element) {
+            // Tutaj this wskazuje na element Window, bo funkcja została
+            //wywołana bez żadnego kontekstu.
+            console.log("innerFuncOne: ", element);
+            element.style.backgroundColor = "plum";
+            // document.getElementById("b").style.backgroundColor = "red";
+        }
 
-    function innerFuncOne() {
-      // Tutaj this wskazuje na element Window, bo funkcja została
-      //wywołana bez żadnego kontekstu.
-      console.log("innerFuncOne: ", this);
-    }
-    innerFuncOne();
+        innerFuncOne(this);
 
-  });
+    });
 
-})
+});
