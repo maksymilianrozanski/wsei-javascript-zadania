f73a2a48bbf9995d3eac060f08e837485871280d
Max <maksymilianrozanski@gmail.com>
2019-10-23 21:50:11 +0200

    solve Zadanie02/12_DOM_Eventy

--- 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie02.js
@@ -1,1 +1,15 @@
-//TODO: solve
+function randomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+EventTarget.prototype.changeColor = function () {
+    this.parentElement.style.backgroundColor = randomColor();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hi");
+    document.body.addEventListener("click", (ev) => {
+        ev.target.changeColor()
+    })
+});
+
