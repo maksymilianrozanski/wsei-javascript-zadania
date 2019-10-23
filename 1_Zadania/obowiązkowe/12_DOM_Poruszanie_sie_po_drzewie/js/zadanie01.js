6019f82c5f42667096398d0c6aa10d773c28b307
Max <maksymilianrozanski@gmail.com>
2019-10-23 21:11:08 +0200

    use this in Zadanie01/12_DOM_Eventy

--- 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie01.js
@@ -1,12 +1,20 @@
+EventTarget.prototype.changeVisibility = changeVisibility;
+
 document.addEventListener("DOMContentLoaded", () => {
-    Array.from(document.body.children).map(it => it.addEventListener("click", (ev => {
-        let next = ev.target.nextElementSibling;
-        if (next !== null && next.tagName !== "SCRIPT") {
-            if (next.style.visibility === 'visible' || next.style.visibility === "") {
-                next.style.visibility = 'hidden'
-            } else {
-                next.style.visibility = 'visible'
-            }
+    document.body.addEventListener("click",
+        (ev) => {
+            ev.target.changeVisibility()
         }
-    })))
+    );
 });
+
+function changeVisibility() {
+    let next = this.nextElementSibling;
+    if (next !== null && next.tagName !== "SCRIPT") {
+        if (next.style.visibility === 'visible' || next.style.visibility === "") {
+            next.style.visibility = 'hidden'
+        } else {
+            next.style.visibility = 'visible'
+        }
+    }
+}
