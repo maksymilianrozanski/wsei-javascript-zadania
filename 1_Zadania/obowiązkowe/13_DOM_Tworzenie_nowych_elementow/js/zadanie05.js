855d501a7dd14444b6909f419a59a7a3ea2e2282
Max <maksymilianrozanski@gmail.com>
2019-10-24 11:55:41 +0200

    solve 05/13_DOM_Tworzenie

---
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie05.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let list1 = document.getElementById("list1");
+    let list2 = document.getElementById("list2");
+
+    list1.addEventListener("click", ev => {
+        if (ev.target.className === "moveBtn") {
+            let liElement = ev.target.parentElement;
+            moveToAnotherList(liElement)
+        }
+    });
+
+    list2.addEventListener("click", ev => {
+        if (ev.target.className === "moveBtn") {
+            let liElement = ev.target.parentElement;
+            moveToAnotherList(liElement)
+        }
+    });
+
+    function moveToAnotherList(liElement) {
+        if (liElement.parentElement.id === "list1") {
+            liElement.parentElement.removeChild(liElement);
+            list2.appendChild(liElement)
+        } else {
+            liElement.parentElement.removeChild(liElement);
+            list1.appendChild(liElement);
+        }
+    }
+});
