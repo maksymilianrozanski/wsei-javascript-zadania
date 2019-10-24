0cccbff4531bcc6b963dc6f2073fe4e509a83602
Max <maksymilianrozanski@gmail.com>
2019-10-24 10:46:31 +0200

    solve Zadanie00/11_DOM_Eventy

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -1,1 +1,16 @@
-//TODO: add solution
+document.addEventListener("DOMContentLoaded", () => {
+
+    Array.from(document.getElementsByClassName("parent"))
+        .forEach(div => {
+            div.addEventListener("mouseover", showChildren);
+            div.addEventListener("mouseout", hideChildren)
+        });
+
+    function showChildren() {
+        this.children[0].style.display = "block";
+    }
+
+    function hideChildren() {
+        this.children[0].style.display = "none"
+    }
+});
