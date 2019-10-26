039cbb515066a21fc5fb5f89fc5f578fd0363124
Max <maksymilianrozanski@gmail.com>
2019-10-26 21:26:08 +0200

    solve dodatkowe zestaw 7 zad 2

--- 1_Zadania/dodatkowe/7/js/app.js
+++ 1_Zadania/dodatkowe/7/js/app.js
@@ -6,6 +6,25 @@ function getNumber(number, array) {
 console.log(getNumber(2, [33, 54, 2, 1, 4, 100]));
 console.log(getNumber(5, [33, 54, 2, 1, 4, 100]));
 
+//Zadanie 2
+function createIdentityMatrix(rows, columns) {
+    let array = [];
+    for (let i = 0; i < rows; i++) {
+        let row = [];
+        for (let j = 0; j < columns; j++) {
+            if (i === j) {
+                row.push(1)
+            } else {
+                row.push(0)
+            }
+        }
+        array.push(row)
+    }
+    return array
+}
+
+console.log(createIdentityMatrix(4, 4));
+
 //Zadanie 1 - dodatkowe
 
 function addTheSameNumbers(number, array) {
