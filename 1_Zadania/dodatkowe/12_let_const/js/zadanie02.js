67c11c6669c025740db1b63c776b98c7c61f0341
Max <maksymilianrozanski@gmail.com>
2019-10-27 12:26:04 +0100

    solve dodatkowe zestaw 12 zad 02

--- 1_Zadania/dodatkowe/12_let_const/js/zadanie02.js
+++ 1_Zadania/dodatkowe/12_let_const/js/zadanie02.js
@@ -6,8 +6,10 @@ const lover = 'Shrek';
     let timeOfDay = "noc";
     let character = "ogrzyca";
     console.log("Teraz jest: " + timeOfDay + " i Fiona to: " + character + ", a jej ukochany to: " + lover);
-    // TODO: Napisz, co kryje się pod zmiennymi i wyjaśnij dlaczego.
+    //Napisz, co kryje się pod zmiennymi i wyjaśnij dlaczego.
+    //Wartości zadeklarowane wewnątrz bloku {}, brane są pod uwagę najpierw zmienne o mniejszym zasięgu
 }
 
 console.log("Teraz jest: " + timeOfDay + " i Fiona to: " + character + ", a jej ukochany to: " + lover);
-// TODO: Napisz, co kryje się pod zmiennymi i wyjaśnij dlaczego.
+//Napisz, co kryje się pod zmiennymi i wyjaśnij dlaczego.
+//Wartości zadeklarowane przed blokiem {}, w tym miejscu nie ma dostępu do zmiennych zadeklarowanych wewnątrz bloku
