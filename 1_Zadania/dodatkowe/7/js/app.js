a2a397b2cefd5c8cae02ae3920ac9c65019bb1f8
Max <maksymilianrozanski@gmail.com>
2019-10-27 11:29:53 +0100

    solve dodatkowe zestaw 7 zad 4 - dodatkowe

--- 1_Zadania/dodatkowe/7/js/app.js
+++ 1_Zadania/dodatkowe/7/js/app.js
@@ -72,3 +72,15 @@ function getMissingElement(array) {
 console.log(getMissingElement([1, 2, 3, 4, 5, 6, 7]));
 console.log(getMissingElement([6, 7, 8, 10, 11, 12, 13, 14, 15]));
 console.log(getMissingElement([-4, -3, -2, 0, 1, 2, 3, 4]));
+
+//Zadanie 4 - dodatkowe
+function getLastNumbers(number, array) {
+    if (isNaN(number)) return [];
+    let startIndex = array.length - number;
+    return array.slice(startIndex)
+}
+
+console.log(getLastNumbers(2, [1, 2, 3, 4, 5, 6, 7]));
+console.log(getLastNumbers(4, [6, 7, 8, 10, 11, 12, 13, 14, 15]));
+console.log(getLastNumbers([-4, -3, -2, 0, 1, 2, 3, 4]));
+console.log(getLastNumbers('ala', [-4, -3, -2, 0, 1, 2, 3, 4]));
