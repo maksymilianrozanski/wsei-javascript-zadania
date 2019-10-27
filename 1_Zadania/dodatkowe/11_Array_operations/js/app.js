90d5b5a947026cb0a20173bbffcb66758b116812
Max <maksymilianrozanski@gmail.com>
2019-10-27 11:45:20 +0100

    solve dodatkowe zestaw 11 zad 0

--- 1_Zadania/dodatkowe/11_Array_operations/js/app.js
+++ 1_Zadania/dodatkowe/11_Array_operations/js/app.js
@@ -1,7 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function (callbackfn) {
     var numbers = [5, 10, 12, 20, 100];
     var result = numbers.reduce(function (prev, curr, index, array) {
         return prev * curr;
-    })
+    });
     console.log(result);
-})
+
+    //Zadanie 0
+    let cities = ["Kraków", "Olsztyn", "Szczecin", "Ostrów Wielkopolski"];
+    let nameLengths = cities.reduce((array, element) => {
+        array.push(element.length);
+        return array;
+    }, []);
+    console.log(nameLengths);
+
+});
