bf49655077fd48b67f7e823503907157dfee0d2e
Max <maksymilianrozanski@gmail.com>
2019-10-28 16:09:29 +0100

    solve dodatkowe zestaw 13 zad 08

--- 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie08.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie08.js
@@ -1,10 +1,10 @@
 const dog = {
     type: "Mammal",
     name: "",
-    setName: (newName) => {
+    setName: function (newName) {
         this.name = newName;
     }
-}
+};
 
 dog.setName("Reksio");
 console.log(dog.name);
