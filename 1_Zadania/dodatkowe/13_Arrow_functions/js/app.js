5ec075c593c1550023d7e9f46cfa65ab78cb2de4
Max <maksymilianrozanski@gmail.com>
2019-10-28 15:52:40 +0100

    solve dodatkowe zestaw 13 zad 02

--- 1_Zadania/dodatkowe/13_Arrow_functions/js/app.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/js/app.js
@@ -19,3 +19,8 @@ console.log(students.numberOfLetters); // [4, 5, 7]
 //Zadanie 1
 const helloWorld = () => console.log("Hello World");
 helloWorld();
+
+//Zadanie 2
+const multiplyByTwo = (number) => typeof number !== "undefined" ? number * 2 : 1;
+console.log(multiplyByTwo(3));
+console.log(multiplyByTwo());
