e9864f7031cb110ed05cc9afe95e5ad596be9b78
Max <maksymilianrozanski@gmail.com>
2019-10-30 10:50:25 +0100

    solve dodatkowe zestaw 13 zad 09

--- 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie09.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie09.js
@@ -1,10 +1,18 @@
-const dog = {
-    type: "Mammal",
-    name: "",
-    setName: (newName) => {
-        this.name = newName;
-    }
-}
+document.addEventListener("DOMContentLoaded", () => {
 
-dog.setName("Reksio");
-console.log(dog.name);
+    //hide <ul> elements where parent is <div> element
+    Array.from(document.getElementsByTagName("UL")).filter(
+        it => it.parentElement.tagName === 'DIV'
+    ).forEach(it => it.style.display = "none");
+
+    Array.from(document.getElementsByTagName("SPAN"))
+        .forEach(it => {
+            it.addEventListener("mouseover", (ev => {
+                ev.target.nextSibling.nextSibling.style.display = "block"
+            }));
+
+            it.addEventListener("mouseleave", (ev => {
+                ev.target.nextSibling.nextSibling.style.display = "none"
+            }))
+        })
+});
