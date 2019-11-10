7c22227e889904b70047a8388378dce07e247104
Max <maksymilian.rozanski@gmail.com>
2019-11-10 20:04:49 +0100

    add price to array

--- 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -3,7 +3,7 @@ function Basket() {
     this.products = [];
     this.sum = 0;
     this.addProduct = function (name, price) {
-        this.products.push(name);
+        this.products.push({name, price});
         this.sum += price;
     };
     this.showBasket = function () {
