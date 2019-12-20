aedf314e00cd379e5c13e561bd585d4a23e8331f
Max <maksymilian.rozanski@gmail.com>
2019-12-20 18:10:29 +0100

    solve 15_1

--- 1_Zadania/dodatkowe/15_Rest_spread_operator/js/app.js
+++ 1_Zadania/dodatkowe/15_Rest_spread_operator/js/app.js
@@ -7,5 +7,12 @@ function getAverage() {
     return Array.from(arguments).reduce((acc, current) =>
           acc + current, 0)/ arguments.length
 }
-console.log(getAverage(2,4,5,6,7,79))
-console.log(getAverage(2,4,6))
+console.log(getAverage(2,4,5,6,7,79));
+console.log(getAverage(2,4,6));
+
+//Zadanie 1
+
+let name = "Maksymilian";
+let nameArray = [...name];
+
+console.log(nameArray);
